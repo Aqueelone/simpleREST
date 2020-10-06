11aa126b7f637fe00de1a5e487186337c7ca2bb2
Aqueelone <aqueelone@gmail.com>
2020-10-06 20:56:32 +0300

    implement Hibernate with H2 in-memory

--- src/main/java/pro/keenetic/okoswaroga/api/controller/PetController.java
+++ src/main/java/pro/keenetic/okoswaroga/api/controller/PetController.java
@@ -1,8 +1,7 @@
 package pro.keenetic.okoswaroga.api.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import pro.keenetic.okoswaroga.api.entity.model.NewPet;
-import pro.keenetic.okoswaroga.api.entity.model.Pet;
+import pro.keenetic.okoswaroga.api.entity.model.PetEntry;
 import pro.keenetic.okoswaroga.api.service.PetService;
 
 import java.util.List;
@@ -10,11 +9,10 @@ import java.util.List;
 import static spark.Spark.*;
 
 public class PetController {
-    private final PetService petService = new PetService();
     private final ObjectMapper om = new ObjectMapper();
     private String welcome = "";
 
-    public PetController(int port, String welcome) {
+    public PetController(int port, String welcome, PetService petService) {
         this.welcome = welcome;
         // Start embedded server at this port
         port(port);
@@ -26,16 +24,16 @@ public class PetController {
         post("/pet/add", (request, response) -> {
             String name = request.queryParams("name");
             String tag = request.queryParams("tag");
-            NewPet pet = petService.add(name, tag);
+            PetEntry pet = petService.add(name, tag);
             response.status(201); // 201 Created
-            return om.writeValueAsString(pet);
+            return pet;
         });
 
         // GET - Give me pet with this id
         get("/pet/:id", (request, response) -> {
-            Pet pet = petService.findById(request.params(":id"));
+            PetEntry pet = petService.findById(request.params(":id"));
             if (pet != null) {
-                return om.writeValueAsString(pet);
+                return pet;
             } else {
                 response.status(404); // 404 Not found
                 return om.writeValueAsString("pet not found");
@@ -44,39 +42,38 @@ public class PetController {
 
         // Get - Give me all pets
         get("/pet", (request, response) -> {
-            List<Pet> result = petService.findAll();
+            List<PetEntry> result = petService.findAll();
             if (result.isEmpty()) {
                 return om.writeValueAsString("pet not found");
             } else {
-                return om.writeValueAsString(petService.findAll());
+                return result;
             }
         });
 
         // PUT - Update pet
         put("/pet/:id", (request, response) -> {
             String id = request.params(":id");
-            Pet pet = petService.findById(id);
-            if (pet != null) {
-                String name = request.queryParams("name");
-                String tag = request.queryParams("tag");
-                petService.update(id, name, tag);
+            String name = request.queryParams("name");
+            String tag = request.queryParams("tag");
+
+            PetEntry pet = petService.findById(id);
+            if (pet != null && petService.update(id, name, tag)) {
                 return om.writeValueAsString("pet with id " + id + " is updated!");
             } else {
                 response.status(404);
-                return om.writeValueAsString("pet not found");
+                return om.writeValueAsString("pet not updated");
             }
         });
 
         // DELETE - delete pet
         delete("/pet/:id", (request, response) -> {
             String id = request.params(":id");
-            Pet pet = petService.findById(id);
-            if (pet != null) {
-                petService.delete(id);
+            PetEntry pet = petService.findById(id);
+            if (pet != null && petService.delete(id)) {
                 return om.writeValueAsString("pet with id " + id + " is deleted!");
             } else {
                 response.status(404);
-                return om.writeValueAsString("pet not found");
+                return om.writeValueAsString("pet not deleted");
             }
         });
     }
