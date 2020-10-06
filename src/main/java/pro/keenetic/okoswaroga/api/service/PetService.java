5b6449dfc84baa66f552be175c9352f965738cc1
Aqueelone <aqueelone@gmail.com>
2020-10-06 11:29:36 +0300

    added PetController and main application class

---
+++ src/main/java/pro/keenetic/okoswaroga/api/service/PetService.java
@@ -0,0 +1,30 @@
+package pro.keenetic.okoswaroga.api.service;
+
+import pro.keenetic.okoswaroga.api.entity.model.NewPet;
+import pro.keenetic.okoswaroga.api.entity.model.Pet;
+
+import java.util.List;
+
+public class PetService {
+    public PetService() {
+    }
+
+    public NewPet add(String name, String tag) {
+        return null;
+    }
+
+    public Pet findById(String params) {
+        return null;
+    }
+
+    public List<Pet> findAll() {
+        return null;
+    }
+
+    public void update(String id, String name, String tag) {
+    }
+
+    public void delete(String id) {
+
+    }
+}
